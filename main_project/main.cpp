20b7c50bfe1e3b0c6f71430382e1e6adf9962095
Overloade1212 <oveerlord139@gmail.com>
2024-05-20 23:10:09 +0300

    Добавлены тестовые данные

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -4,6 +4,6 @@ int main() {
     setlocale(LC_ALL, "RU");
     cout << "My variant:6"<<endl;
     cout << "My name is Anatoly" << endl;
-    cout << "My surname is Kolotovkin" << endl;
+    cout << "My surname is Kolotovkin " << endl;
     return 0;
 }
