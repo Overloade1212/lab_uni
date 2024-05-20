80243ea249b69ecab20d950b671ff5c44eae4159
Overloade1212 <oveerlord139@gmail.com>
2024-05-20 22:10:39 +0300

    Добавлен вывод общей информации о задании

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
+    setlocale (LC_ALL,"RU");
     cout << "Hello, World!" << endl;
+    cout << "Мой номер варианта:6"<<endl;
+    cout << "Мое имя Анатолий";
+    cout << "Моя фамилия Колотовкин";
     return 0;
 }
