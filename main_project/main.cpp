c262f3472a2fbaf2a856f71ea4e9c4f559f49843
Overloade1212 <oveerlord139@gmail.com>
2024-05-20 23:13:00 +0300

    Добавлены тестовые данные

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -5,5 +5,6 @@ int main() {
     cout << "My variant:6"<<endl;
     cout << "My name is Anatoly" << endl;
     cout << "My surname is Kolotovkin " << endl;
+    cout << "My group 23ICIT1D " << endl;
     return 0;
 }
