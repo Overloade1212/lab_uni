aee9bd2921ba1de0a15908011a81e3d7deb79f61
Overloade1212 <oveerlord139@gmail.com>
2024-05-21 10:02:21 +0300

    Разрешён конфликт слияния

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "RU");
     cout << "My variant:6"<<endl;
-    cout << "My name is Anatoly" << endl;
+    cout << "My name is Anatoly " << endl;
     cout << "My surname is Kolotovkin " << endl;
     cout << "My group 23ICIT1D " << endl;
     return 0;
